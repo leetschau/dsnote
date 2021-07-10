3fad0bcd3bdcd66572f4c988f9aadae306ba67ff
Li Chao <clouds@isrc.iscas.ac.cn>
2021-07-10 20:01:19 +0800

    some type design about advanced search

--- incubator/fsharp/Notes.fs
+++ incubator/fsharp/Notes.fs
@@ -13,17 +13,33 @@ let DEFAULT_REC_NO = 5
 
 type Note = {
     Title: string
-    Tags: string list
+    TagList: string list
     Notebook: string
     Created: DateTime
     Updated: DateTime
     Content: string
     FilePath: string }
 
+type SearchItem =
+    | Title of string
+    | TagList of string list
+    | Notebook of string
+    | Created of DateTime
+    | Updated of DateTime
+    | Content of string
+
+type SearchFlag =
+    | TextFlag of ignoreCase : bool * wholeWord : bool
+    | DateFlag of beforeDate : bool
+
+type SearchTerm =
+    { Body: SearchItem
+      Flag: SearchFlag option }
+
 let parseNote (note: string): Note =
     let lines = File.ReadAllLines note
     { Title = lines.[0].[7..]
-      Tags = lines.[1].Split "; " |> Array.toList
+      TagList = lines.[1].Split "; " |> Array.toList
       Notebook = lines.[2].[10..]
       Created = lines.[3].[9..] |> DateTime.Parse
       Updated = lines.[4].[9..] |> DateTime.Parse
@@ -44,11 +60,82 @@ let saveAndDisplayNotes (notes: Note list): string =
 let simpleSearch (args: string list): string =
     let wordInNote (word: string) (note: Note): bool =
         (note.Content.Contains word) || (note.Title.Contains word) ||
-            (List.exists (fun (tag: string) -> tag.Contains word) note.Tags)
-    let notes = List.fold (fun noteList word -> noteList |> List.filter(wordInNote word))
+            (List.exists (fun (tag: string) -> tag.Contains word) note.TagList)
+    let notes = List.fold (fun noteList word -> noteList |> List.filter (wordInNote word))
                           (loadNotes NOTE_REPO)
                           args
     saveAndDisplayNotes notes
+
+let advancedSearch (args: string list): string =
+    let parseTerm (term: string): SearchTerm option =
+        let terms = term.Split(":") |> Array.toList
+        let baseTerm: SearchTerm option =
+            match terms with
+            | [ "ti"; word ]
+            | [ "ti"; word; _ ] ->
+                Some({ Body = Title(word); Flag = None })
+            | [ "ta"; words ]
+            | [ "ta"; words; _ ] ->
+                Some({ Body = TagList(words.Split(",") |> Array.toList); Flag = None })
+            | [ "nb"; word ]
+            | [ "nb"; word; _ ] ->
+                Some({ Body = Notebook(word); Flag = None })
+            | [ "cr"; date ]
+            | [ "cr"; date; _ ] ->
+                Some({ Body = Created(DateTime.Parse date); Flag = None })
+            | [ "up"; date ]
+            | [ "up"; date; _ ] ->
+                Some({ Body = Updated(DateTime.Parse date); Flag = None })
+            | t ->
+                printfn "Invalid search term: %A" t
+                None
+
+        match baseTerm with
+        | Some(baseT) & (Some({ Body = Title(_); Flag = _ }) | Some({ Body = TagList(_); Flag = _ }) | Some({ Body = Notebook(_); Flag = _ })) ->
+            match terms with
+            | [ _; _ ]
+            | [ _; _; "iW" ]
+            | [ _; _; "Wi" ]
+            | [ _; _; "i" ]
+            | [ _; _; "W" ] ->
+                Some({ baseT with Flag = Some(TextFlag(ignoreCase = true,
+                                                     wholeWord = false)) })
+            | [ _; _; "iw" ]
+            | [ _; _; "wi" ]
+            | [ _; _; "w" ] ->
+                Some({ baseT with Flag = Some(TextFlag(ignoreCase = true,
+                                                     wholeWord = true)) })
+            | [ _; _; "Iw" ]
+            | [ _; _; "wI" ] ->
+                Some({ baseT with Flag = Some(TextFlag(ignoreCase = false,
+                                                     wholeWord = true)) })
+            | [ _; _; "IW" ]
+            | [ _; _; "WI" ]
+            | [ _; _; "I" ] ->
+                Some({ baseT with Flag = Some(TextFlag(ignoreCase = false,
+                                                     wholeWord = false)) })
+            | _ -> None
+        | Some(baseT) & (Some({ Body = Created(_); Flag = _ }) | Some( { Body = Updated(_); Flag = _ })) ->
+            match terms with
+            | [ _; _; "b" ] ->
+                Some({ baseT with Flag = Some(DateFlag(beforeDate = true)) })
+            | [ _; _; "B" ] ->
+                Some({ baseT with Flag = Some(DateFlag(beforeDate = false)) })
+            | [ _; _; flag ] ->
+                printfn "Invalid search style: %s" flag
+                Some({ baseT with Flag = None })
+            | _ -> None
+        | _ -> None
+
+    let noteOnTerm (term: SearchTerm option) (note: Note): bool =
+        true
+
+    let notes = List.fold (fun noteList term -> noteList |> List.filter (noteOnTerm term))
+                          (loadNotes NOTE_REPO)
+                          (List.map parseTerm args)
+    saveAndDisplayNotes notes
+
+
 
 let listNotes (num: int): string =
     (loadNotes NOTE_REPO |> List.sortByDescending (fun note -> note.Updated)).[..num - 1] |> saveAndDisplayNotes
