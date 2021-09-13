83d7673e530156d40bb275b66177f4c1f2d9a8d1
Li Chao <clouds@isrc.iscas.ac.cn>
2021-09-13 22:49:50 +0800

    more on rust version

--- incubator/ron/src/main.rs
+++ incubator/ron/src/main.rs
@@ -1,24 +1,20 @@
 use std::env;
+mod notes;
 
 fn main() {
-    let arg1: Vec<String> = env::args().collect();
-    let args: Vec<&str> = arg1.iter().map(|s| s as &str).collect();
+    let arg_sv: Vec<String> = env::args().collect();
+    let args: Vec<&str> = arg_sv.iter().map(|s| s as &str).collect();
     match &args[1..] {
         [] => println!("empty vec"),
-        ["s", end @ ..] => {
-            //println!("Start: {:?}", start);
-            println!("Searching terms: {:?}", end)
+        ["s", words @ ..] => {
+            println!("{:?}", notes::simple_search(words));
         },
         ["e"] => {
-            edit_note(1);
+            notes::edit_note(1);
         },
         ["e", no] => {
-            edit_note(no.parse::<u32>().unwrap());
+            notes::edit_note(no.parse::<u32>().unwrap());
         },
         _ => println!("Invalid command!"),
     }
-}
-
-fn edit_note(note_no: u32) {
-    println!("Edit note No. {}", note_no);
 }
